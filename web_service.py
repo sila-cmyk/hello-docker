4d209b092acf82c1bd049dc8303c29eed957f4b9
sila-cmyk <slazorlu224@gmail.com>
2025-11-20 14:04:29 +0300

    Update web_service.py

--- web_service.py
+++ web_service.py
@@ -1,20 +1,21 @@
 from flask import Flask, render_template, request, redirect
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 # Mesajları geçici bir listede tutuyoruz (veritabanı yerine)
-mesajlar = []
+mesajlar = [] 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET","POST"])
 def index():
     if request.method == "POST":
         isim = request.form.get("isim")
-        mesaj = request.form.get("mesaj")
-        if isim and mesaj:
-            mesajlar.append({"isim": isim, "mesaj": mesaj})
-        return redirect("/")
+        mesaj = request.method == "POST":
+                isim = request.form.get("isim")
+                isim = request.form.get("mesaj")
+                if isim and mesaj:
+                    mesajlar.append({"isim": isim, "mesaj": mesaj})
+                return redirect("/")
 
-    return render_template("index.html", mesajlar=mesajlar)
-
-if _name_ == "_main_":
-    app.run(host="0.0.0.0", port=5000)
+            if__name__== "__main__":
+                app.run(host="0.0.0.0", port=5000)
+        
